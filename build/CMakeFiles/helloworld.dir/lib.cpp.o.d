fc5ebe33bf5c77af0fd351ac29d4451222b3431c
Максим <bidnik.maxim@yandex.ru>
2025-02-09 16:45:56 +0300

    Test2

---
+++ build/CMakeFiles/helloworld.dir/lib.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/helloworld.dir/lib.cpp.o: \
+ /home/corsac/Project/helloworld_cli/lib.cpp /usr/include/stdc-predef.h \
+ /home/corsac/Project/helloworld_cli/lib.h \
+ /home/corsac/Project/helloworld_cli/build/version.h
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/corsac/Project/helloworld_cli
+# Build directory: /home/corsac/Project/helloworld_cli/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_version "test_version")
+set_tests_properties(test_version PROPERTIES  _BACKTRACE_TRIPLES "/home/corsac/Project/helloworld_cli/CMakeLists.txt;74;add_test;/home/corsac/Project/helloworld_cli/CMakeLists.txt;0;")
